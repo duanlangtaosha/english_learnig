a77d4725a4dc695f8c4a764fce295e0743ea27ef
duanlangtaosha <891650837@qq.com>
2019-04-24 07:41:05 +0800

    添加主题的子模块

--- source/conf.py
+++ source/conf.py
@@ -76,7 +76,14 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#更换第三方的主题
+html_theme = 'sphinx_rtd_theme'
+
+#为True则用 '''如 :numref:`xxx` 会显示如图/表x '''
+numfig = True
+
+#上一层目录
+html_theme_path = ['../sphinx_rtd_theme'] 
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
